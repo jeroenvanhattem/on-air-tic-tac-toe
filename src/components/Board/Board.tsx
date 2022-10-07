807504a01b5fd41a0cda92ae07816b61c1a4fd92
jeroenvanhattem <jeroenvanhattem@outlook.com>
2022-10-07 20:07:24 +0200

    Check win algorithm working for dynamic grid sizes

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,28 +1,50 @@
 import { useEffect, useState } from "react"
 import { checkWin } from "../../functions/checkWin"
 import { BoardType } from "../../types/Board"
-import "./Board.scss"
+import styles from "./Board.module.scss"
 
 export const Board = () => {
+
+  // MOVE ALL OF THIS TO REDUX 
+
   const [testBoard, setTestBoard] = useState<any>([
-    ['x', 'x', 'x'],
-    ['o', '', 'o'],
-    ['o', 'x', 'o']
+    ['o', 'x', 'x'],
+    ['o', 'x', 'o'],
+    ['x', 'o', 'x'],
   ])
 
-  const [currentMove, setCurrentMove] = useState<string>('x')
-
+  const [currentMover, setCurrentMover] = useState<string>('x')
+  const [moves, setMoves] = useState<any>(0) // [index of cell, mover]
+  const [gridSize, setGridSize] = useState<number>(3)
 
   useEffect(() => {
     if (testBoard) {
-      const result = checkWin({ board: testBoard })
+      const result = checkWin({ board: testBoard, currentMover })
       console.log('Result: ', result)
+      console.log(result ? `You won as ${currentMover}` : `You lost as ${currentMover}`)
     }
   }, [testBoard])
 
   return (
-    <div className="Board">
-      Board
-    </div>
+    <div className={styles.Board}>
+      {[...Array(gridSize)].map((_, i) => {
+        return (
+          <div className={styles.Row}>
+            {
+              [...Array(gridSize)].map((_, j) => {
+                return (
+                  <div
+                    className={styles.Cell}
+                    onClick={() => { console.log('Clicked cell: ', i, j) }}
+                  >
+                  </div>
+                )
+              })
+            }
+          </div >
+        )
+      })
+      }
+    </div >
   )
 }
--- src/functions/checkWin.ts
+++ src/functions/checkWin.ts
@@ -1,16 +1,137 @@
 import { BoardType } from "../types/Board";
 
-export const checkWin = ({ board }: { board: BoardType | any }) => {
+export const checkWin = ({
+	board,
+	currentMover,
+}: {
+	board: BoardType | any;
+	currentMover: string;
+}) => {
 	console.log(board);
 	const gridSize = board.length;
 	console.log("gridSize", gridSize);
-	return true;
+
+	let win = false;
+	win = checkRow({ board, currentMover });
+	console.log("Row win:", win);
+
+	if (!win) {
+		win = checkColumn({ board, currentMover });
+		console.log("Column win:", win);
+	}
+
+	if (!win) {
+		win = checkDiagonal({ board, currentMover });
+		console.log("Diagonal win:", win);
+	}
+
+	if (!win) {
+		win = checkInvertedDiagonal({ board, currentMover });
+		console.log("Inverted diagonal win:", win);
+	}
+
+	return win;
 };
 
-const checkRow = ({ board }: { board: BoardType | any }) => {};
+const checkRow = ({
+	board,
+	currentMover,
+}: {
+	board: BoardType | any;
+	currentMover: string;
+}) => {
+	const gridSize = board.length;
 
-const checkColumn = ({ board }: { board: BoardType | any }) => {};
+	let win = false;
 
-const checkDiagonal = ({ board }: { board: BoardType | any }) => {};
+	for (let i = 0; i < gridSize; i++) {
+		let row = board[i];
+		let allThis = true;
+		for (let j = 0; j < gridSize; j++) {
+			if (row[j] !== currentMover) {
+				allThis = false;
+				break;
+			}
+		}
+		if (allThis) {
+			win = true;
+		}
+	}
 
-const checkInvertedDiagonal = ({ board }: { board: BoardType | any }) => {};
+	return win;
+};
+
+const checkColumn = ({
+	board,
+	currentMover,
+}: {
+	board: BoardType | any;
+	currentMover: string;
+}) => {
+	const gridSize = board.length;
+	let win = false;
+
+	for (let i = 0; i < gridSize; i++) {
+		let allThis = true;
+		for (let j = 0; j < gridSize; j++) {
+			if (board[j][i] !== currentMover) {
+				allThis = false;
+				break;
+			}
+		}
+		if (allThis) {
+			win = true;
+		}
+	}
+
+	return win;
+};
+
+const checkDiagonal = ({
+	board,
+	currentMover,
+}: {
+	board: BoardType | any;
+	currentMover: string;
+}) => {
+	const gridSize = board.length;
+
+	let win = false;
+	let allThis = true;
+
+	for (let i = 0; i < gridSize; i++) {
+		if (board[i][i] !== currentMover) {
+			allThis = false;
+			return false;
+		}
+		if (allThis) {
+			win = true;
+		}
+	}
+
+	return win;
+};
+
+const checkInvertedDiagonal = ({
+	board,
+	currentMover,
+}: {
+	board: BoardType | any;
+	currentMover: string;
+}) => {
+	const gridSize = board.length;
+	let win = false;
+
+	let allThis = true;
+	for (let i = 0; i < gridSize; i++) {
+		if (board[gridSize - 1 - i][i] !== currentMover) {
+			allThis = false;
+			return false;
+		}
+		if (allThis) {
+			win = true;
+		}
+	}
+
+	return win;
+};
